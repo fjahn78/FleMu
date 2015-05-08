832e2e0c6c0d08680a379bf88e2f2c42ed3ea7c0
Frank Schumann <fjahn78+github@gmail.com>
2015-05-08 17:34:49 +0200

    Fix Ups

--- index.php
+++ index.php
@@ -4,18 +4,19 @@
 <meta charset="utf-8">
 <title>FleMu</title>
 <link href="css/main.css" rel="stylesheet" type="text/css">
+<!--The following script tag downloads a font from the Adobe Edge Web Fonts server for use within the web page. We recommend that you do not modify it.--><script>var __adobewebfontsappname__="dreamweaver"</script><script src="http://use.edgefonts.net/source-sans-pro:n6:default.js" type="text/javascript"></script>
 </head>
 
 <body>
-<div id="contentwrap">
-  <div id="wrapper">
-    <header id="header">
-      <h1>FleMu</h1>
-    </header>
+<div id="wrapper">
+  <header id="header">
+    <h1>FleMu</h1>
+  </header>
+  <div id="contentwrap">
     <div id="logoart"><img src="images/logo.gif"  alt=""/></div>
     <aside id="loginform">Raum für den Inhalt von  id "loginform"</aside>
   </div>
+  <footer>Raum für den Inhalt des neuen footer-Tags</footer>
 </div>
-<footer>Raum für den Inhalt des neuen footer-Tags</footer>
 </body>
 </html>
