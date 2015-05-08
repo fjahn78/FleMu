fc541605ac7722ce22f2360868ca4dea391ba3da
Frank Schumann <fjahn78+github@gmail.com>
2015-05-08 16:29:35 +0200

    Skeleton Entry Page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>FleMu</title>
+<link href="css/main.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div id="contentwrap">
+  <div id="wrapper">
+    <header id="header">
+      <h1>FleMu</h1>
+    </header>
+    <div id="logoart"><img src="images/logo.gif"  alt=""/></div>
+    <aside id="loginform">Raum für den Inhalt von  id "loginform"</aside>
+  </div>
+</div>
+<footer>Raum für den Inhalt des neuen footer-Tags</footer>
+</body>
+</html>
